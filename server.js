a4ae5497205216426abcc6164eeecc73b99f80bc
Andrew P <andrewpetersondev@gmail.com>
2019-07-25 03:06:14 -0500

    added dependencies, express config, middleware, router info, listener

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 // dependencies
-// =====================================================
+// ============================================================
 var path = require('path');
 var express = require('express');
+
+// express 
+// ============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// express middleware
+// ============================================================
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+// router
+// ============================================================
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// listener
+// ============================================================
+app.listen(PORT, function(){
+    console.log("App is listening on PORT: " + PORT);
+});

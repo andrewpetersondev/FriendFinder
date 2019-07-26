086e22a6a01c5c593c147e91e39cc17822506d38
Andrew P <andrewpetersondev@gmail.com>
2019-07-25 21:45:47 -0500

    added meaningful values to scores in friends object array

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,12 +2,12 @@ var friends = [
     {
         name: "Peter Quill",
         photo: "https://images.pexels.com/photos/556667/pexels-photo-556667.jpeg?cs=srgb&dl=backlit-blur-couple-556667.jpg&fm=jpg",
-        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     },
     {
         name: "Peter Parker",
         photo: "https://media1.popsugar-assets.com/files/thumbor/0QwGtTXU5fFxHgdSNO1yDfefvE8/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/07/03/141/n/1922283/tmp_mCokKS_a76fb1674f68f442_MCDSPFA_EC022.jpg",
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        scores: [1, 2, 3, 3, 4, 5, 5, 3, 3, 3]
     },
     {
         name: "Tony Stark",
@@ -17,17 +17,17 @@ var friends = [
     {
         name: "Natasha Romanov",
         photo: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/011blw_ons_inl_02.jpg",
-        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+        scores: [, 3, 3, 3, 3, 3, 3, 3, 3, 3]
     },
     {
         name: "Bruce Banner",
         photo: "https://img1.looper.com/img/gallery/how-hulk-and-bruce-banner-could-settle-their-differences-in-avengers-4/intro-1535641451.jpg",
-        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+        scores: [1, 5, 2, 4, 3, 1, 5, 2, 4, 3]
     },
     {
         name: "Peggy Carter",
         photo: "https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/PeggyCarter2.jpg/220px-PeggyCarter2.jpg",
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1]
+        scores: [4, 4, 4, 4, 4, 4, 4, 4, 4]
     }
 ];
 
